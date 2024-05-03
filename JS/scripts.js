19a8b54d77371f3fed4fd1b75bad224d30897911
Lukas Sysel <40641535+lukassysel@users.noreply.github.com>
2024-05-03 22:09:39 +0200

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,17 @@
 const playerNameElement = document.getElementsByClassName("playerName")[0];
 const playerScoreElement = document.getElementsByClassName("playerScore")[0];
+const botNameElement = document.getElementsByClassName("botName")[0];
 const botScoreElement = document.getElementsByClassName("botScore")[0];
 const startGameButton = document.getElementsByClassName("startGame")[0];
 const resetScreen = document.getElementsByClassName("reset")[0];
 const whoWon = document.getElementById("whoWon");
 const resetBtn = document.getElementById("resetBtn");
+const width = window.innerWidth;
+const height = window.innerHeight;
+const s5 = document.getElementsByClassName("5s")[0];
+const s8 = document.getElementsByClassName("8s")[0];
+const s13 = document.getElementsByClassName("13s")[0];
+const s20 = document.getElementsByClassName("20s")[0];
     
 const btn1 = document.getElementsByClassName("btn-1")[0];
 const btn2 = document.getElementsByClassName("btn-2")[0];
@@ -15,27 +22,77 @@ const btn6 = document.getElementsByClassName("btn-6")[0];
 const btn7 = document.getElementsByClassName("btn-7")[0];
 const btn8 = document.getElementsByClassName("btn-8")[0];
 const btn9 = document.getElementsByClassName("btn-9")[0];
-
-btnArr = [0,0,0,0,0,0,0,0,0]
-arrOfBtns = [btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9]
+var toggleBotBtn = document.getElementsByClassName("toggle-input")[0];
+let toggleButton = toggleBotBtn.checked;
+let btnArr = [0,0,0,0,0,0,0,0,0]
+let arrOfBtns = [btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9]
+let arrOfTimes = [s5,s8,s13,s20]
 
 const playerTimer = document.getElementsByClassName("playerTimer")[0];
 const botTimer = document.getElementsByClassName("botTimer")[0];
 let turn;   //0 = bot, 1 = player
-let playerCountdown = 10 
-let botCountdown = 10
-let firstStart = 1
+
+let setTime = 13
+let playerCountdown = setTime;
+let botCountdown = setTime;
+let botWaitTime = 1083;
+
 let playerCircle = document.getElementsByClassName("playerIcon")[0];
 let botCircle = document.getElementsByClassName("botIcon")[0];
-let setTime = 10
+let user2 = 1; //0 = bot or 1=player
+
 
 //TODO
-
 //input for player 1 and player 2 names
-//input for time (3, 5, 7, 10, 20)
+
+function chooseTime(){
+    for(let i = 0; i < arrOfTimes.length; i++){
+        arrOfTimes[i].addEventListener('click', function() {
+            for(let j = 0; j < arrOfTimes.length; j++){
+                arrOfTimes[j].style.border = "none"
+            }
+            arrOfTimes[i].style.border = "3px solid rgb(113, 223, 80)"
+            setTime = arrOfTimes[i].innerHTML
+            playerCountdown = setTime
+            botCountdown = setTime
+            playerTimer.innerHTML = setTime
+            botTimer.innerHTML = setTime
+            botWaitTime = Math.floor((arrOfTimes[i].innerHTML * 1000) / 12)
+        })
+    }
+}
+s13.style.border = "3px solid rgb(113, 223, 80)"
+chooseTime()
+
+toggleBotBtn.addEventListener('change', function() {
+    toggleButton = this.checked;
+    if(toggleButton == false){
+        user2 = 1
+        botNameElement.innerHTML = "Player 2"
+    }else{
+        user2 = 0
+        botNameElement.innerHTML = "SHIT ON BOT"
+    }
+});
 
 playerTimer.innerHTML = setTime
 botTimer.innerHTML = setTime
+
+for(let i = 0; i < arrOfBtns.length; i++){
+    BtnOnOff(arrOfBtns[i], 0)
+}
+
+function BtnOnOff(button, state){ //on = 1, off = 0
+    if(state == 1){
+        button.disabled = false;
+        button.classList.add("startGame_hover")
+        button.classList.add("startGame_active")
+    }else{
+        button.disabled = true;
+        button.classList.remove("startGame_hover")
+        button.classList.remove("startGame_active")
+    }
+}
 
 function randomPersonStarts(){
     let n = Math.floor(Math.random() * 2)
@@ -61,6 +118,13 @@ function startPlayerCountdown() {
             checkTime()
             whichPlayer(1)
             checkWiner()
+            for(let i = 0; i < arrOfBtns.length; i++){
+                if(btnArr[i] == 0){
+                    BtnOnOff(arrOfBtns[i], 1)
+                }else{
+                    BtnOnOff(arrOfBtns[i], 0)
+                }
+            }
         } else{
             clearInterval(playerCountDownInterval)
         }
@@ -77,6 +141,22 @@ function startBotCountdown() {
             checkTime()
             whichPlayer(0)
             checkWiner()
+            if(user2 == 1){
+                for(let i = 0; i < arrOfBtns.length; i++){
+                    if(btnArr[i] == 0){
+                        BtnOnOff(arrOfBtns[i], 1)
+                    }else{
+                        BtnOnOff(arrOfBtns[i], 0)
+                    }
+                } 
+            }else{
+                for(let i = 0; i < arrOfBtns.length; i++){
+                    BtnOnOff(arrOfBtns[i], 0)
+                }
+            }
+            const botMove = setTimeout(() => {
+                bot()
+            }, botWaitTime);
         } else{
             clearInterval(botCountDownInterval)
         }
@@ -96,189 +176,50 @@ function startLoop(){
     randomPersonStarts()
     startPlayerCountdown()
     startBotCountdown()
-    disableGameStartBtn()
+    BtnOnOff(startGameButton, 0)
+    BtnOnOff(toggleBotBtn, 0)
 }
 function whichPlayer(person){
-    if(person == 0){
-        botCircle.style.border = "8px solid #18BC9C"
-        playerCircle.style.border = "2px solid #18BC9C"
-    } else if(person == 1){
-        playerCircle.style.border = "8px solid #18BC9C"
-        botCircle.style.border = "2px solid #18BC9C"
+    if(width <= 768){
+        if(person == 0){
+            botCircle.style.border = "4px solid #18BC9C"
+            playerCircle.style.border = "1px solid #18BC9C"
+        } else if(person == 1){
+            playerCircle.style.border = "4px solid #18BC9C"
+            botCircle.style.border = "1px solid #18BC9C"
+        }
+    }else{
+        if(person == 0){
+            botCircle.style.border = "8px solid #18BC9C"
+            playerCircle.style.border = "2px solid #18BC9C"
+        } else if(person == 1){
+            playerCircle.style.border = "8px solid #18BC9C"
+            botCircle.style.border = "2px solid #18BC9C"
+        }
     }
 }
-function btn1Place(person){
-    if(person == 0 && btn1.innerHTML !== "X" && btn1.innerHTML !== "O"){
-        btn1.innerHTML = "O"
+
+function btnPlace(btn, position){
+    if(turn == 0 && btn.innerHTML !== "X" && btn.innerHTML !== "O"){
+        btn.innerHTML = "O"
         turn = 1
-        btnArr[0] = -4
+        btnArr[position] = magicCube[position] * -1
         startPlayerCountdown()
-    }else if(person == 1 && btn1.innerHTML !== "O" && btn1.innerHTML !== "X"){
-        btn1.innerHTML = "X"
+    }else if(turn == 1 && btn.innerHTML !== "O" && btn.innerHTML !== "X"){
+        btn.innerHTML = "X"
         turn = 0
-        btnArr[0] = 4
+        btnArr[position] = magicCube[position]
         startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
+    }else{
+        console.log("Button on position " + position + " is already filled")
     }
 }
-
-btn1.addEventListener("click", function() {
-    btn1Place(turn)
-})
-function btn2Place(person){
-    if(person == 0 && btn2.innerHTML !== "X" && btn2.innerHTML !== "O"){
-        btn2.innerHTML = "O"
-        turn = 1
-        btnArr[1] = -9
-        startPlayerCountdown()
-    }else if(person == 1 && btn2.innerHTML !== "O" && btn2.innerHTML !== "X"){
-        btn2.innerHTML = "X"
-        turn = 0
-        btnArr[1] = 9
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
+for(let i = 0; i < arrOfBtns.length; i++){
+    arrOfBtns[i].addEventListener("click", function() {
+        btnPlace(arrOfBtns[i], i)
+    })
 }
 
-btn2.addEventListener("click", function() {
-    btn2Place(turn)
-})
-function btn3Place(person){
-    if(person == 0 && btn3.innerHTML !== "X" && btn3.innerHTML !== "O"){
-        btn3.innerHTML = "O"
-        turn = 1
-        btnArr[2] = -2
-        startPlayerCountdown()
-    }else if(person == 1 && btn3.innerHTML !== "O" && btn3.innerHTML !== "X"){
-        btn3.innerHTML = "X"
-        turn = 0
-        btnArr[2] = 2
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn3.addEventListener("click", function() {
-    btn3Place(turn)
-})
-function btn4Place(person){
-    if(person == 0 && btn4.innerHTML !== "X" && btn4.innerHTML !== "O"){
-        btn4.innerHTML = "O"
-        turn = 1
-        btnArr[3] = -3
-        startPlayerCountdown()
-    }else if(person == 1 && btn4.innerHTML !== "O" && btn4.innerHTML !== "X"){
-        btn4.innerHTML = "X"
-        turn = 0
-        btnArr[3] = 3
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn4.addEventListener("click", function() {
-    btn4Place(turn)
-})
-function btn5Place(person){
-    if(person == 0 && btn5.innerHTML !== "X" && btn5.innerHTML !== "O"){
-        btn5.innerHTML = "O"
-        turn = 1
-        btnArr[4] = -5
-        startPlayerCountdown()
-    }else if(person == 1 && btn5.innerHTML !== "O" && btn5.innerHTML !== "X"){
-        btn5.innerHTML = "X"
-        turn = 0
-        btnArr[4] = 5
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn5.addEventListener("click", function() {
-    btn5Place(turn)
-})
-function btn6Place(person){
-    if(person == 0 && btn6.innerHTML !== "X" && btn6.innerHTML !== "O"){
-        btn6.innerHTML = "O"
-        turn = 1
-        btnArr[5] = -7
-        startPlayerCountdown()
-    }else if(person == 1 && btn6.innerHTML !== "O" && btn6.innerHTML !== "X"){
-        btn6.innerHTML = "X"
-        turn = 0
-        btnArr[5] = 7
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn6.addEventListener("click", function() {
-    btn6Place(turn)
-})
-
-function btn7Place(person){
-    if(person == 0 && btn7.innerHTML !== "X" && btn7.innerHTML !== "O"){
-        btn7.innerHTML = "O"
-        turn = 1
-        btnArr[6] = -8
-        startPlayerCountdown()
-    }else if(person == 1 && btn7.innerHTML !== "O" && btn7.innerHTML !== "X"){
-        btn7.innerHTML = "X"
-        turn = 0
-        btnArr[6] = 8
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn7.addEventListener("click", function() {
-    btn7Place(turn)
-})
-function btn8Place(person){
-    if(person == 0 && btn8.innerHTML !== "X" && btn8.innerHTML !== "O"){
-        btn8.innerHTML = "O"
-        turn = 1
-        btnArr[7] = -1
-        startPlayerCountdown()
-    }else if(person == 1 && btn8.innerHTML !== "O" && btn8.innerHTML !== "X"){
-        btn8.innerHTML = "X"
-        turn = 0
-        btnArr[7] = 1
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn8.addEventListener("click", function() {
-    btn8Place(turn)
-})
-function btn9Place(person){
-    if(person == 0 && btn9.innerHTML !== "X" && btn9.innerHTML !== "O"){
-        btn9.innerHTML = "O"
-        turn = 1
-        btnArr[8] = -6
-        startPlayerCountdown()
-    }else if(person == 1 && btn9.innerHTML !== "O" && btn9.innerHTML !== "X"){
-        btn9.innerHTML = "X"
-        turn = 0
-        btnArr[8] = 6
-        startBotCountdown()
-    }else {
-        console.log("turn is undefined or btn is already filled")
-    }
-}
-
-btn9.addEventListener("click", function() {
-    btn9Place(turn)
-})
 
 function checkWithoutDraw(){ //returns 0=bot wins, returns 1=player wins
     switch (btnArr[0] + btnArr[1] + btnArr[2]){
@@ -372,7 +313,8 @@ function gameEnd(){
     turn = 2
     playerCircle.style.border = "2px solid #18BC9C"
     botCircle.style.border = "2px solid #18BC9C"
-    disableGameStartBtn()
+    BtnOnOff(startGameButton, 0)
+    BtnOnOff(toggleBotBtn, 1)
     resetScreen.style.display = "flex"
 }
 function disableGameStartBtn(){
@@ -380,11 +322,7 @@ function disableGameStartBtn(){
     startGameButton.classList.remove("startGame_hover")
     startGameButton.classList.remove("startGame_active")
 }
-function activateGameStartBtn(){
-    startGameButton.disabled = false
-    startGameButton.classList.add("startGame_hover")
-    startGameButton.classList.add("startGame_active")
-}
+
 function resetGame(){
     for(let i = 0; i < btnArr.length; i++){
         btnArr[i] = 0
@@ -393,9 +331,72 @@ function resetGame(){
     playerCountdown = setTime
     botCountdown = setTime
     resetScreen.style.display = "none"
+    for(let i = 0; i < arrOfBtns.length; i++){
+        BtnOnOff(arrOfBtns[i], 0)
+    }
     startLoop()
 }
 resetBtn.addEventListener("click", function() {
     resetGame()
 })
 
+
+let magicCube = [4,9,2,
+                 3,5,7,
+                 8,1,6]
+
+function botWin() {
+    if(turn == 0){
+        for (let i = 0; i < btnArr.length; i++) {
+            if (btnArr[i] == 0) {
+                btnArr[i] = magicCube[i] * -1;
+                if (checkWithoutDraw() == 0) {
+                    arrOfBtns[i].innerHTML = "O";
+                    checkWiner()
+                    return(true);
+                } else{
+                    btnArr[i] = 0
+                }
+            }
+        }
+    }
+}
+function botBlock() {
+    if(turn == 0){
+        for (let i = 0; i < btnArr.length; i++) {
+            if (btnArr[i] == 0) {
+                btnArr[i] = magicCube[i];
+                if (checkWithoutDraw() == 1) {
+                    btnPlace(arrOfBtns[i], i);
+                    return(true);
+                } else{
+                    btnArr[i] = 0
+                }
+            }
+        }
+    }
+}
+function botRandom() {
+    listOfI = []
+    for(let i = 0; i < btnArr.length; i++){
+        if(btnArr[i] == 0){
+            listOfI.push(i)
+        }
+    }
+    let n = Math.floor(Math.random() * listOfI.length)
+    btnPlace(arrOfBtns[listOfI[n]], listOfI[n])
+    checkWiner()
+}
+function bot(){
+    if(user2 == 0){
+        botWin()
+        if(turn == 0){
+            botBlock() 
+        }
+        if(turn == 0){
+            botRandom()
+        }
+    }  
+}
+
+bot()
